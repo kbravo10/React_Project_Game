6a8b1e161bc1966eb0e3227c4a26abc950a9d94b
kevinBravo <kbravo1024@gmail.com>
2024-02-25 11:00:31 -0800

    add img

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,7 +2,7 @@ function Game(){
     return(
         <div>
             <h1>MOLEMAN</h1>
-            <p1>pokemon</p1>
+            <img src="https://previews.123rf.com/images/rastudio/rastudio1109/rastudio110900081/10614002-cartoon-character-mole.jpg"/>
         </div>
 
     );
