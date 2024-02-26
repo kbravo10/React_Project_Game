972e20da3093e8328bbf5fc28a6c101901165079
kevinBravo <kbravo1024@gmail.com>
2024-02-26 08:33:33 -0800

    resize image main css

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,7 +2,7 @@ function Game(){
     return(
         <div>
             <h1>MOLEMAN</h1>
-            <img alt="oops" src="https://st2.depositphotos.com/1007989/5894/i/950/depositphotos_58947201-stock-illustration-miner-mole-in-hard-hat.jpg"/>
+            <img className="moleImage" alt="oops" src="https://st2.depositphotos.com/1007989/5894/i/950/depositphotos_58947201-stock-illustration-miner-mole-in-hard-hat.jpg"/>
         </div>
 
     );
