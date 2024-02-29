aa8e1aa90e21aa397a63d35bc6e6ba58196ef305
kevinBravo <kbravo1024@gmail.com>
2024-02-28 21:09:23 -0800

    add score component

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,11 +1,8 @@
-import { useState } from "react";
 
 function Score(){
-    const [score, setScore] = useState(0)
     return(
         <div>
             <h1>Score</h1>
-            <span>{score}</span>
         </div>
 
     )
